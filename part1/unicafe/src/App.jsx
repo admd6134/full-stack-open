e6c7d3863b2e255e4c00d40502e29fbdae2aa886
admd6134 <admd6134@gmail.com>
2025-01-05 18:15:10 +0200

    solve exercise 1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,15 +10,19 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [average, setAverage] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1);
+    setAverage((good + 1 - bad) / (good + 1 + bad + neutral))
   };
   const handleNeutral = () => {
     setNeutral(neutral + 1);
+    setAverage((good - bad) / (good + bad + neutral + 1))
   };
   const handleBad = () => {
     setBad(bad + 1);
+    setAverage((good - bad - 1) / (good + bad + 1 + neutral))
   };
   return (
     <div>
@@ -33,6 +37,12 @@ const App = () => {
       neutral {neutral}
       <br />
       bad {bad}
+      <br />
+      all {good + bad + neutral}
+      <br />
+      average {average}
+      <br />
+      positive {(good - bad) / good}
     </div>
   );
 };
