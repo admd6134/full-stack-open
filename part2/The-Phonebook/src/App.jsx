3f95bd04e2d8fe43db9dabda7c5eda84f9d32fea
admd6134 <admd6134@gmail.com>
2025-01-07 12:57:51 +0200

    solve exercise 2.7: The Phonebook Step 2

--- part2/The-Phonebook/src/App.jsx
+++ part2/The-Phonebook/src/App.jsx
@@ -6,6 +6,10 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (JSON.stringify(persons).includes(JSON.stringify({ name: newName }))) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
     setPersons(persons.concat({ name: newName }));
     setNewName("");
   };
